af3921139309644d5ee8140d8506b85102b43ec1
toxichl <472590061@qq.com>
2017-04-01 05:59:44 +0800

    Simple Update

--- app/components/ArticleData.js
+++ app/components/ArticleData.js
@@ -16,19 +16,53 @@ class ArticleModel {
 
 }
 
-let ArticleData = new Array()
+// let ArticleData = new Array()
+//
+// for (let i = 0; i < 4; i++) {
+// 	ArticleData.push(new ArticleModel(
+// 		{
+// 			link: '',
+// 			img: SUFFIX + (i > 7 ? i - 7 : i + 1) + '.jpg',
+// 			title: 'Food &amp; Drink',
+// 			subtitle: "We Eat and Drink All Night",
+// 			date: "March 6th, 2016"
+// 		}
+// 	))
+// }
 
-for (let i = 0; i < 16; i++) {
-	ArticleData.push(new ArticleModel(
-		{
-			link: '',
-			img: SUFFIX + (i > 7 ? i - 7 : i + 1) + '.jpg',
-			title: 'Food &amp; Drink',
-			subtitle: "We Eat and Drink All Night",
-			date: "March 6th, 2016"
-		}
-	))
-}
+let ArticleData = [
+	{
+		link: '',
+		img: `${SUFFIX}2.jpg`,
+		title: 'Coding',
+		subtitle: "Life-long learning",
+		date: "2016"
+	},
+	{
+		link: '',
+		img: `${SUFFIX}1.jpg`,
+		title: 'Writing',
+		subtitle: "Life-long writing.",
+		date: "2007"
+	},
+	{
+		link: '',
+		img: `${SUFFIX}3.jpg`,
+		title: 'Clip',
+		subtitle: "Life-long learning",
+		date: "2016"
+	},
+	{
+		link: '',
+		img: `${SUFFIX}4.jpg`,
+		title: 'Singing',
+		subtitle: "Life-long learning",
+		date: "2016"
+	}
+]
+
+
+
 
 
 export default ArticleData;
