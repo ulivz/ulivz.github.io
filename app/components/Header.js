67343b2e89b56408ccf2e248253a72347a6cd3a7
toxichl <472590061@qq.com>
2017-04-01 04:57:09 +0800

    Simple Update

--- app/components/Header.js
+++ app/components/Header.js
@@ -11,12 +11,12 @@ class Header extends React.Component {
 					<div className="row">
 						<a href="#" className="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
 						<ul className="fh5co-social">
+							<li><a href="https://github.com/toxichl"><i className="icon-github"></i></a></li>
+							<li><a href="http://weibo.com/1631967413/profile"><i className="icon-sina-weibo"></i></a></li>
 							<li><a href="#"><i className="icon-twitter"></i></a></li>
-							<li><a href="#"><i className="icon-facebook"></i></a></li>
-							<li><a href="#"><i className="icon-instagram"></i></a></li>
 						</ul>
 						<div className="col-lg-12 col-md-12 text-center">
-							<h1 id="fh5co-logo"><a href="index.html">Magazine <sup>TM</sup></a></h1>
+							<h1 id="fh5co-logo"><a href="index.html">Toxichl <sup>TM</sup></a></h1>
 						</div>
 
 					</div>
