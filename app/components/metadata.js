f38ed144e9eebe132e7dd54a452c5893f888ac04
toxichl <472590061@qq.com>
2017-04-01 04:29:17 +0800

    Simple Update

--- app/components/metadata.js
+++ app/components/metadata.js
@@ -3,11 +3,11 @@ const SUFFIX = "static/images/pic_"
 class ArticleModel {
 
 	constructor(opts = {
-		            link : '',
-		            img : '',
-		            title : '',
-		            subtitle : '',
-		            date : ''
+		            link: '',
+		            img: '',
+		            title: '',
+		            subtitle: '',
+		            date: ''
 	            }) {
 
 		Object.assign(this, opts)
@@ -22,7 +22,7 @@ for (let i = 0; i < 16; i++) {
 	metadata.push(new ArticleModel(
 		{
 			link: '',
-			img: SUFFIX + Math.floor(i/2) + 1,
+			img: SUFFIX + (i > 7 ? i - 7 : i + 1) + '.jpg',
 			title: 'Food &amp; Drink',
 			subtitle: "We Eat and Drink All Night",
 			date: "March 6th, 2016"
