db12916497acc2ea9d3f4e308ecd0d430b3f0a54
toxichl <472590061@qq.com>
2017-04-01 04:11:28 +0800

    Simple Update

---
+++ app/components/metadata.js
@@ -0,0 +1,11 @@
+const SUFFIX = "static/images/pic_"
+
+export default metadata = [
+	{
+		link: "",
+		img: `${SUFFIX}1`,
+		title: 'Food &amp; Drink',
+		subtitle: "We Eat and Drink All Night",
+		date: "March 6th, 2016"
+	}
+]
