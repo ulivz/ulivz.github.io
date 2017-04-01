eaa091002e0a83e8f94c0ee0f27409e93114ed96
toxichl <472590061@qq.com>
2017-04-01 18:28:39 +0800

    Simple Update

---
+++ app/components/Detail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ArticalData from './ArticleData.js';
+import ClearFix from './ClearFix.js';
+import Article from './Article.js';
+
+class Content extends React.Component {
+
+	render() {
+		return (
+			<div className="container-fluid">
+				Detail
+			</div>
+		);
+	}
+
+}
+
+export {Content as default};
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,10 +4,8 @@ import {
 	Route,
 	Link
 } from 'react-router-dom'
-
-import Shop from '../components/Shop';
-import Study from '../components/Study';
-import Video from '../components/Video';
+import Content from '../components/Content.js'
+import Detail from '../components/Detail.js'
 
 class MyRouter extends React.Component {
 
@@ -16,19 +14,8 @@ class MyRouter extends React.Component {
 		return (
 			<Router>
 				<div>
-					<div>
-						<h1 className="comment-title">React Router</h1>
-						<div className="ui divider"></div>
-					</div>
-					<div className="nav">
-						<div className="three-colums"><Link to="/shop">Shop</Link></div>
-						<div className="three-colums"><Link to="/study">Study</Link></div>
-						<div className="three-colums"><Link to="/video">Video</Link></div>
-					</div>
-					<Route exact path="/" component={Shop}/>
-					<Route path="/shop" component={Shop}/>
-					<Route path="/study" component={Study}/>
-					<Route path="/video" component={Video}/>
+					<Route exact path="/" component={Content}/>
+					<Route path="/detail" component={Detail}/>
 				</div>
 			</Router>
 		)
