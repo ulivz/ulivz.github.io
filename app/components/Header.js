e6ca113f497e94017f629aa4e3f3cb18e62b2a20
toxichl <472590061@qq.com>
2017-04-01 18:01:35 +0800

    Simple Update

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,9 +5,7 @@ class Header extends React.Component {
 	render() {
 		return (
 			<header id="fh5co-header">
-
 				<div className="container-fluid">
-
 					<div className="row">
 						<a href="#" className="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
 						<ul className="fh5co-social">
@@ -15,7 +13,11 @@ class Header extends React.Component {
 							<li><a href="http://weibo.com/1631967413/profile"><i className="icon-sina-weibo"></i></a></li>
 						</ul>
 						<div className="col-lg-12 col-md-12 text-center">
-							<h1 id="fh5co-logo"><a href="index.html">Toxichl <sup>TM</sup></a></h1>
+							<h1 id="fh5co-logo">
+								{/*<a href="index.html">*/}
+									{/*V2JS <sup>~</sup></a>*/}
+								<img width="100" src="static/images/logo.svg" alt=""/>
+							</h1>
 						</div>
 
 					</div>
