2928180fb8c00ff9c5d7b2f7b1b1485e44d0512f
toxichl <472590061@qq.com>
2017-04-03 01:11:18 +0800

    Simple Update

--- app/index.js
+++ app/index.js
@@ -8,6 +8,8 @@ import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import Content from './components/Content.js';
 
+
+
 import MyRouter from './routes/routes.js';
 
 ReactDOM.render(
