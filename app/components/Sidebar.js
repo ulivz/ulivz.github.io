2c02a4c2f51b0191f627da0859864ac6148c43b5
toxichl <472590061@qq.com>
2017-04-03 01:22:36 +0800

    Simple Update

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 class Sidebar extends React.Component {
 
@@ -8,21 +9,17 @@ class Sidebar extends React.Component {
 				<a href="#" className="v2js-close-offcanvas js-v2js-close-offcanvas"><span>
 				<i className="icon-cross3"></i> <span>Close</span></span></a>
 				<div className="v2js-bio">
-					<figure>
-						<img src="https://avatars0.githubusercontent.com/u/23133919?v=3&u=dc5a09ee6220ece04f379319f13686669a2b3c4f&s=400" alt="Free HTML5 Bootstrap Template" className="img-responsive" />
-					</figure>
+					<a href="/">
+						<figure>
+							<img src="https://avatars0.githubusercontent.com/u/23133919?v=3&u=dc5a09ee6220ece04f379319f13686669a2b3c4f&s=400" alt="Free HTML5 Bootstrap Template" className="img-responsive" />
+						</figure>
+					</a>
 					<h3 className="heading">About Me</h3>
 					<h2>Toxichl</h2>
 					<p>A Coder & Designer</p>
 					<ul className="v2js-social">
 						<li><a href="https://github.com/toxichl"><i className="icon-github"></i></a></li>
 						<li><a href="http://weibo.com/1631967413/profile"><i className="icon-sina-weibo"></i></a></li>
-						{/*<li><a href="#"><i className="icon-twitter"></i></a></li>
-						<li><a href="#"><i className="icon-github"></i></a></li>
-						<li><a href="#"><i className="icon-google"></i></a></li>
-						<li><a href="#"><i className="icon-"></i></a></li>
-						<li><a href="#"><i className="icon-facebook"></i></a></li>
-						<li><a href="#"><i className="icon-instagram"></i></a></li>*/}
 					</ul>
 				</div>
 
