261a6ac01925fce13d04805436e65f319d3f9508
toxichl <472590061@qq.com>
2017-04-03 00:51:36 +0800

    Simple Update

--- app/components/Header.js
+++ app/components/Header.js
@@ -17,22 +17,6 @@ class Header extends React.Component {
 						</button>
 					</a>
 					<a href="#" className="js-v2js-nav-toggle v2js-nav-toggle"><i></i></a>
-					{/*<ul className="v2js-social">*/}
-						{/*<li><a href="https://github.com/toxichl"><i className="icon-github"></i></a></li>*/}
-						{/*<li><a href="http://weibo.com/1631967413/profile"><i className="icon-sina-weibo"></i></a></li>*/}
-					{/*</ul>*/}
-					{/*<div className="row">*/}
-
-
-						{/*<div className="col-lg-12 col-md-12 text-center">*/}
-							{/*<h1 id="v2js-logo">*/}
-								{/*/!*<a href="index.html">*!/*/}
-								{/*/!*V2JS <sup>~</sup></a>*!/*/}
-								{/*<img width="100" src="static/images/logo.svg" alt=""/>*/}
-							{/*</h1>*/}
-						{/*</div>*/}
-
-					{/*</div>*/}
 				</div>
 			</header>
 		);
